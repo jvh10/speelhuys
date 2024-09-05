0d9641bdbe7a26f95a8a4e00697d898cb6f622dc
Noah Barendsma <nbarendsma@gmail.com>
2024-09-05 11:53:05 +0200

    navbar

--- beheer.php
+++ beheer.php
@@ -4,3 +4,18 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="stylesheet" href="CSS/style.css">
 </head>
+
+<div class="row">
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <a class="navbar-brand" href="index.php">Klantenlijst</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div class="navbar-nav">
+                <a class="nav-item nav-link" href="products.php">Products</a>
+                <a class="nav-item nav-link" href="orders.php">Orders</a>
+            </div>
+        </div>
+    </nav>
+</div>
