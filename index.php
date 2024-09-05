b6cd9e288f11351be0aaeb57a233a07860eb0e4b
Jari van Huizen <jarivanhuizen10@gmail.com>
2024-09-05 11:43:45 +0200

    Login basis

--- index.php
+++ index.php
@@ -1,6 +1,13 @@
 <html>
 <head>
-    <link rel="stylesheet" href="">
+    <link rel="stylesheet" href="style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
-
+<body>
+    <form action="$_POST">
+        <input type="text"> <br>
+        <input type="password"> <br>
+        <button>Sign in</button>
+    </form>
+</body>
 </html>
