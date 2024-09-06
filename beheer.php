55cbf826743a55c9ee708e6b072baf648eb7d5b1
Noah Barendsma <nbarendsma@gmail.com>
2024-09-06 10:26:20 +0200

    filter

--- beheer.php
+++ beheer.php
@@ -21,9 +21,19 @@
             </nav>
         </div>
         <div class="row">
-            <aside data-test="facet-navigation">
-                
-            </aside>
+            <input type="text" id="myInput" onkeyup="myFunction()" placeholder="Search for names..">
+
+            <ul id="myUL">
+                <li><a href="#">Adele</a></li>
+                <li><a href="#">Agnes</a></li>
+
+                <li><a href="#">Billy</a></li>
+                <li><a href="#">Bob</a></li>
+
+                <li><a href="#">Calvin</a></li>
+                <li><a href="#">Christina</a></li>
+                <li><a href="#">Cindy</a></li>
+            </ul>
         </div>
     </div>
 </body>
