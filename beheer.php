b59e02c92d237d8a085673af6e56251cda6d28af
Noah Barendsma <nbarendsma@gmail.com>
2024-09-06 10:43:00 +0200

    javascript en filter update

--- beheer.php
+++ beheer.php
@@ -2,7 +2,8 @@
 
 <head>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <link rel="stylesheet" href="CSS/style.css">
+    <link rel="stylesheet" href="style.css">
+    <script src="style.js" type="text/javascript"></script>
 </head>
 
 <body>
@@ -26,8 +27,39 @@
             <ul id="myUL">
                 <br></br>
                 Brands<br>
-                <input type="radio">Lego
+                <input type="radio"> Lego <br>
+                <input type="radio"> Kapla <br>
+                <input type="radio"> Duplo <br>
+                <input type="radio"> RoboTime <br>
+                <input type="radio"> SmartMax <br>
+                <input type="radio"> Brio <br>
+                <input type="radio"> Playmobil <br>
+                <input type="radio"> MegaBlocks <br>
+                <input type="radio"> MegaConstrux <br>
+                <input type="radio"> Geomag <br>
+                <input type="radio"> Knex <br>
+                <input type="radio"> GraviTrax <br>
+                <input type="radio"> Clementoni <br>
             </ul>
+            <div class="dropdown">
+                <button onclick="myFunction()" class="dropbtn">Dropdown</button>
+                <div id="myDropdown" class="dropdown-content">
+                    <input type="text" placeholder="Search.." id="myInput" onkeyup="filterFunction()">
+                    <input type="radio"> Lego <br>
+                <input type="radio"> Kapla <br>
+                <input type="radio"> Duplo <br>
+                <input type="radio"> RoboTime <br>
+                <input type="radio"> SmartMax <br>
+                <input type="radio"> Brio <br>
+                <input type="radio"> Playmobil <br>
+                <input type="radio"> MegaBlocks <br>
+                <input type="radio"> MegaConstrux <br>
+                <input type="radio"> Geomag <br>
+                <input type="radio"> Knex <br>
+                <input type="radio"> GraviTrax <br>
+                <input type="radio"> Clementoni <br>
+                </div>
+            </div>
         </div>
     </div>
 </body>
