2fb841c6c61cccb9cddaaba19ba31117c73e3aa9
Noah Barendsma <nbarendsma@gmail.com>
2024-09-11 14:41:29 +0200

    connectie

--- connectie.php
+++ connectie.php
@@ -1,12 +1,19 @@
 <?php
+// hier word de connectie gemaakt naar de database
+class Database
+{
+    public static function start()
+    {
+        $dbServername = "127.0.0.1";
+        $dbUsername = "root";
+        $dbPassword = "";
+        $dbDatabase = "speelhuys";
 
-$dbServername = "127.0.0.1";
-$dbUsername = "root";
-$dbPassword = "";
-$dbDatabase = "steptember";
+        $conn = new mysqli($dbServername, $dbUsername, $dbPassword, $dbDatabase);
 
-$conn = new mysqli($dbServername, $dbUsername, $dbPassword, $dbDatabase);
-
-if ($conn->connect_error) {
-    die("connectie mislukt: " . $conn->connect_error);
+        if ($conn->connect_error) {
+            die("Connectie mislukt: " . $conn->connect_error);
+        }
+        return $conn;
+    }
 }
